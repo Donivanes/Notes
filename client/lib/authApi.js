c1acb7b45cf15e3fdb55dbb940d0ff87b5065c52
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-04-04 13:50:05 +0200

    test signup

--- client/lib/authApi.js
+++ client/lib/authApi.js
@@ -3,27 +3,27 @@ import _ from "lodash";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
-  withCredentials: true
+  withCredentials: true,
 });
 
-export const signup = async user => {
+export const signup = async (user) => {
   const { data } = await api.post("/auth/signup", { user });
-  return _.pick(data, "username", "campus", "course");
+  return _.pick(data, "username", "email");
 };
 
 export const login = async (username, password) => {
   const { data } = await api.post("/auth/login", {
     username,
-    password
+    password,
   });
-  return _.pick(data, "username", "campus", "course");
+  return _.pick(data, "username", "email");
 };
 
 export const logout = async () => await api.get("/auth/logout");
 
 export const loggedin = async () => {
   const { data } = await api.get("/auth/loggedin");
-  return _.pick(data, "username", "campus", "course");
+  return _.pick(data, "username", "email");
 };
 
 // export const edit = async () => await api.post("/user/edit", { user });
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
 import React from "react";
 import "./App.css";
+import { HomePage } from "./pages/Home.Page";
+import { SignUpStudentPage } from "./pages/SignUpStudent.Page";
+import { SignUpTeacherPage } from "./pages/SignUpStudent.Page";
 
-export const App = () => <div>Start Project Commit</div>;
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/signupStudent" component={SignUpStudentPage} />
+      <Route path="/signupTeacher" component={SignUpTeacherPage} />
+      {/* <Route path="/login" component={LoginPage} />
+      <Route path="/profile" component={ProfilePage} /> */}
+    </Switch>
+  </Router>
+);
